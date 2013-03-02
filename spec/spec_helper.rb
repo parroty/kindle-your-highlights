ba13ec19036490df356d30314bab77dc07692c56
parroty <parroty00@gmail.com>
2013-03-03 01:25:34 +0900

    refine spec file - use :vcr

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,4 +4,9 @@ VCR.configure do |c|
    c.cassette_library_dir = 'spec/data/vcr'
    c.hook_into :webmock
    c.allow_http_connections_when_no_cassette = true
+   c.configure_rspec_metadata!
+end
+
+RSpec.configure do |c|
+  c.treat_symbols_as_metadata_keys_with_true_values = true
 end
